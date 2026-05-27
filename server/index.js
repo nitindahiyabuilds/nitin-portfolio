45e4defce9ee593faca27d2972d2569eabf4b856
Sujal Garg <sujalgarg@Kunals-MacBook-Air-3.local>
2026-05-27 21:10:17 +0530

    updaate

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import connectdb from '../src/config/db.js'
-import ContactSubmission from './models/ContactSubmission.js'
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import cors from 'cors';
+import connectdb from './config/db.js'; 
+
+import ContactSubmission from './models/ContactSubmission.js';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3001
