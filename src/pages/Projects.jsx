283857ac4a401307b8c668c1adc90822f4f0a5dd
Sujal Garg <sujallgarg@gmail.com>
2026-07-23 15:54:40 +0530

    remove cognify

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -48,16 +48,16 @@ const projects = [
     github: "https://github.com/sujallgarg/portfolio",
 
   },
-  {
-    title: "Cognify",
-    category: "Web App / AI",
-    description: "An AI-powered productivity platform built with modern web technologies.",
-    tags: ["React","Next.js","TypeScript","Node.js","Express", "Postgress", "Tailwind", "Vercel", "AI","Stripe"],
-    image: "https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?q=80&w=1200",
-    link: "https://cognify-wine-chi.vercel.app/",
-    github: "https://github.com/sujallgarg/cognify",
+  // {
+  //   title: "Cognify",
+  //   category: "Web App / AI",
+  //   description: "An AI-powered productivity platform built with modern web technologies.",
+  //   tags: ["React","Next.js","TypeScript","Node.js","Express", "Postgress", "Tailwind", "Vercel", "AI","Stripe"],
+  //   image: "https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?q=80&w=1200",
+  //   link: "https://cognify-wine-chi.vercel.app/",
+  //   github: "https://github.com/sujallgarg/cognify",
 
-  }
+  // }
 ];
 
 const Projects = () => {
